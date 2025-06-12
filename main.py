67f6f3b0ce8b8a3785d615079b7f0a0d442f7127
carlo <lobotomisador@gmail.com>
2025-06-12 09:29:08 -0600

    wip

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 import os
 from pathlib import Path
-from src.utils import find_folders
+from src.utils import find_files, find_folders
 
 RESULTS_DIR = Path("results/")
 RECORDS_DIR = Path("records/")
 SA_RATIOS_DIR = RESULTS_DIR / "saratios"
+DMF_DIR = RESULTS_DIR / "dmfs"
 
 
 # Set page config
@@ -30,8 +33,41 @@ selected_folder = st.sidebar.selectbox(
     disabled=not folders
 )
 
-# Main content area
-st.title("MP Wavelets Analysis Dashboard")
+saratios_dir = SA_RATIOS_DIR / selected_folder
+
+selected_damping = st.sidebar.selectbox(
+    "Select Damping",
+    options=find_files(saratios_dir, only_csv=True),
+    disabled=not selected_folder
+)
+
+saratio = pd.read_csv(saratios_dir / selected_damping, index_col=0)
+dmf = pd.read_csv(DMF_DIR / selected_damping, index_col=0)
+
+df_melted = saratio.reset_index().melt(id_vars=['index'], var_name='Case', value_name='SaRatio')
+df_melted = df_melted.rename(columns={'index': 'T'})
+
+dmf_melted = dmf.reset_index().melt(id_vars=['index'], var_name='Case', value_name='DMF')
+dmf_melted = dmf_melted.rename(columns={'index': 'T'})
+df_melted['DMF'] = dmf_melted['DMF']
+df_melted
+
+fig = px.scatter(df_melted, 
+                 x='SaRatio', 
+                 y='DMF',
+                 color='T',
+                 color_continuous_scale='viridis',
+                 opacity=0.6,
+                 title='SaRatio vs DMF Colored by Period')
+
+fig.update_layout(
+    showlegend=True,
+    plot_bgcolor='white',
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+
 
 if selected_folder and selected_folder != "No folders found":
     st.write(f"Selected folder: {selected_folder}")
